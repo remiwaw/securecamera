186fb0fa3355e4c6c5e6f20a5451e3fafb1ab24b
Remigiusz Wawrzyniak <remigiusz.wawrzyniak@ext.sys.aok.de>
2020-11-14 02:22:25 +0100

    Enter app code screen added

--- app/src/main/java/com/rwawrzyniak/securephotos/MainActivity.kt
+++ app/src/main/java/com/rwawrzyniak/securephotos/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.rwawrzyniak.securephotos
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.main_activity)
+class MainActivity : AppCompatActivity(R.layout.main_activity){
+	override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+		super.onCreate(savedInstanceState, persistentState)
+	}
+
+	override fun onPause() {
+		super.onPause()
+		val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+		val navController = navHostFragment.navController
+		navController.navigate(R.id.action_global_appCodeFragment)
+	}
+}
